20840b91e54041e82697f7654e4693340088cc05
KAYLLANY KETYLLY DA SILVA LIMA <kayllany_lima@estudante.sesisenai.org.br>
2024-11-08 17:31:32 -0300

    ATUALIZADO

--- Spectrum/src/pages/Tela_Principal.jsx
+++ Spectrum/src/pages/Tela_Principal.jsx
@@ -1,47 +1,61 @@
 import React from "react";
 import Navbar from '../components/Navbar';
 import './Tela_Principal.css'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"; 
+// import Slider from 'react-slick';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css"; 
 
 function Tela_Principal() {
 
-  const images = [
+  // const images = [
     
-    "./img/FRASE.png",
-    "./img/frase2.png",
-    "./img/frase3.png",
+  //   "./img/FRASE.png",
+  //   "./img/frase2.png",
+  //   "./img/frase3.png",
  
-   ]
- 
-   const settings = {
-    dots: true, // Adiciona os pontinhos de navegação
-    infinite: true, // Loop infinito no carrossel
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  //  ]
+
+  //  const settings = {
+  //   dots: true, // Adiciona os pontinhos de navegação
+  //   infinite: true, // Loop infinito no carrossel
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  // };
   return (
     <div className='Bore'>
         <Navbar/>
 
         <div className='carrocel'>
+
                 <div className="test">
-                    <Slider {...settings}>
+
+
+                   <img className="carrocel_img" src="frase3.png" alt="" />
+                    {/* <Slider {...settings}>
                                 {images.map((image, index) => (
                                   <div key={index}>
                                     <img src={image} alt={`Slide ${index + 1}`} />
                                   </div>
                                 ))}
-                              </Slider>
+                              </Slider> */}
                 </div>
         </div>
-
+             
               <div className='profissionais'>
-                 <div className="containe_profissional1"></div>
-                 <div className="containe_profissional2"></div>
-                 <div className="containe_profissional3"></div>
+              <p className="TEXTOP">PROFISSIONAL</p>
+                 <div className="containe_profissional1">
+                    <img className="img-profissional" src="Milena Bairro.jpg" alt="" />
+                     <p className="nomes">Milena Bairro</p>
+                 </div>
+                 <div className="containe_profissional2">
+                 <img className="img-profissional" src="Gustavo Souza.jpg" alt="" />
+                 <p className="nomes">Gustavo Souza</p>
+                 </div>
+                 <div className="containe_profissional3">
+                 <img className="img-profissional" src="Luciana Bittencourt.jpg" alt="" />
+                 <p className="nomes">Luciana Bittencourt</p>
+                 </div>
               </div>
      </div>
   )
