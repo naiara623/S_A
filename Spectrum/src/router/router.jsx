a0f020901c3d603bdac5686af045863dfb964250
NAIARA RODRIGUES DA SILVA <naiara_r_silva@estudante.sesisenai.org.br>
2024-11-14 16:27:02 -0300

    atualizações

--- Spectrum/src/router/router.jsx
+++ Spectrum/src/router/router.jsx
@@ -5,7 +5,7 @@ import Login from "../pages/Login";
 import Tela_Profissional from "../pages/Tela_Profissional";
 import Tela_Perfil from "../pages/Tela_Perfil";
 import Tela_Informacoes from "../pages/Tela_Informacoes";
-// import Tela_Principal from "../pages/Tela_Principal";
+
 
 const router = createBrowserRouter([
 
@@ -15,7 +15,8 @@ const router = createBrowserRouter([
     {path: "/perfildeusuario", element: <Tela_Perfil />},
     {path: "/perfildeprofissional", element: <Tela_Profissional />},
     {path: "/teladeinformacoes", element: <Tela_Informacoes />},
-    // {path: "/telaprincipal", element: <Tela_Principal />},
+    {path: "/telaprincipal", element: <Tela_Informacoes />},
+    
     
 ])
 
