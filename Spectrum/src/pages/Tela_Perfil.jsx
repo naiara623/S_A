4af37ddb85767910e5cb769125a0248ee9c24833
KAYLLANY KETYLLY DA SILVA LIMA <kayllany_lima@estudante.sesisenai.org.br>
2024-11-21 17:25:58 -0300

    Atualizado

--- Spectrum/src/pages/Tela_Perfil.jsx
+++ Spectrum/src/pages/Tela_Perfil.jsx
@@ -4,34 +4,38 @@ import './Tela_Perfil.css'
 function Perfil() {
   return (
     <div className='body-perfil'>
-       <Navbar/>
+      <Navbar />
 
-     <div className='azul'></div>
+      <div className='azul'></div>
 
-     <div className='laranja'>
+      <div className='laranja'>
 
-          <div className='containe_um'>
-             <img className='keisy' src="keisy.png" alt="" />
-             <div className='blak'></div>
-            
-                <div className='Informações'>
-                  <label htmlFor="">Nome:</label><br />
-                  <label htmlFor="">Idade:</label><br />
-                  <label htmlFor="">Gênero:</label><br />
-                  <label htmlFor="">Email:</label><br />
-                  <label htmlFor="">Número:</label><br />
+        <div className='containe_um'>
+          <img className='keisy' src="keisy.png" alt="" />
 
-                <div className='kkk'></div>
-               </div>
+          <button className='botperfil'></button>
+          
+          <div className='blak'></div>
+          <div className='Informações'>
+            <p className='sobre-mim'>Sobre mim</p>
+            <br />
+            <label className='info'>Nome:</label><br />
+            <label className='info'>Idade:</label><br />
+            <label className='info'>Gênero:</label><br />
+            <label className='info'>Email:</label><br />
+            <label className='info'>Número:</label><br />
+
+            <div className='kkk'></div>
           </div>
+        </div>
 
-      <div className='containe_dois'></div>
+        <div className='containe_dois'></div>
 
-      <div className='containe_três'></div>
-     </div>
+        <div className='containe_três'></div>
+      </div>
 
 
-     <div className='Verde'></div>
+      <div className='Verde'></div>
 
     </div>
   )
