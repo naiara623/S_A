bcc91b951791b4636d48cc206b4ebcea206dfb0d
FERNANDA CARDOSO MARCIANO <fernanda_marciano@estudante.sesisenai.org.br>
2024-11-22 14:31:17 -0300

    Atualizando

--- Spectrum/src/estrategias/Estrategia1.jsx
+++ Spectrum/src/estrategias/Estrategia1.jsx
@@ -11,30 +11,30 @@ function Estrategia1() {
 
 <h2>Sons Altos</h2>
 <p>Lidar com sons altos pode ser particularmente desafiador para pessoas autistas, mas algumas estratégias podem ajudar a minimizar o desconforto:</p>
-<div class="estrategias">
-<ul className='colunas'>
+
+<ul className='colunas1'>
   
-<li className='colunas' ><button href="1.1 Protetores auriculares.html">Protetores auriculares</button></li><br />
-<li className='colunas' ><button href="2.1 Espaços tranquilos.html">Espaços tranquilos</button></li><br/>
-<li className='colunas' ><button href="3.1 Planejamento.html">Planejamento</button></li><br/>
-<li className='colunas' ><button href="4.1 Comunicação.html">Comunicação</button></li><br/>
-<li className='colunas' ><button href="5.1 Técnicas de relaxamento.html">Técnicas de relaxamento</button></li><br/>
-<li className='colunas' ><button href="6.1 Música ou ruído branco.html">Música ou ruído branco</button></li><br/>
-<li className='colunas' ><button href="7.1 Treinamento de dessensibilização.html">Treinamento de dessensibilização</button></li><br/>
+<button href="1.1 Protetores auriculares.html"><li className='colunas2'/>Protetores auriculares</button><br />
+<button href="2.1 Espaços tranquilos.html"><li className='colunas2'/>Espaços tranquilos</button><br/>
+<button href="3.1 Planejamento.html"><li className='colunas2'/>Planejamento</button><br/>
+<button href="4.1 Comunicação.html"><li className='colunas2'/>Comunicação</button><br/>
+<button href="5.1 Técnicas de relaxamento.html"><li className='colunas2'/>Técnicas de relaxamento</button><br/>
+<button href="6.1 Música ou ruído branco.html"><li className='colunas2'/>Música ou ruído branco</button><br/>
+<button href="7.1 Treinamento de dessensibilização.html"><li className='colunas2'/>Treinamento de dessensibilização</button><br/>
 </ul>
 
-</div>
 <p>Cada pessoa é única, então pode ser necessário experimentar diferentes estratégias para encontrar o que funciona melhor em cada caso.</p>
 
 </div>
+
 <div class="sidebar">
       
 <h3>Estratégias para:</h3>
-<ul className='colunas'>
-<li className='colunas'> <button className='estrategias' href="Estratégia 1 Sons Altos.html">Sons Altos</button></li>
-<li className='colunas'> <button className='estrategias' href="Estratégia 2 Mudança na Rotina.html">Mudança na Rotina</button></li>
-<li className='colunas'> <button className='estrategias' href="Estratégia 3 Rotas diferentes do previsto.html">Rotas diferentes do previsto</button></li>
-<li className='colunas'> <button className='estrategias' href="Estratégia 4 Muitas pessoas em um ambiente.html">Muitas pessoas em um ambiente</button></li>
+<ul className='colunas1'>
+ <button className='estrategias' ><li className='colunas3'/>Sons Altos</button><br /><br />
+ <button className='estrategias' ><li className='colunas3'/>Mudança na Rotina</button><br /><br />
+ <button className='estrategias' ><li className='colunas3'/>Rotas diferentes do previsto</button><br /><br />
+ <button className='estrategias' ><li className='colunas3'/>Muitas pessoas em um ambiente</button><br /><br />
 </ul>
 
 </div>
