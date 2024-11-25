b5591d976bf670903a06f2e512705de1d77cea37
FERNANDA CARDOSO MARCIANO <fernanda_marciano@estudante.sesisenai.org.br>
2024-11-25 15:02:38 -0300

    Atualizando

--- Spectrum/src/estrategias/Estrategia1.jsx
+++ Spectrum/src/estrategias/Estrategia1.jsx
@@ -9,33 +9,44 @@ function Estrategia1() {
 
 <div class="container">
 
-<h2>Sons Altos</h2>
-<p>Lidar com sons altos pode ser particularmente desafiador para pessoas autistas, mas algumas estratégias podem ajudar a minimizar o desconforto:</p>
-
-<ul className='colunas1'>
-  
-<button href="1.1 Protetores auriculares.html"><li className='colunas2'/>Protetores auriculares</button><br />
-<button href="2.1 Espaços tranquilos.html"><li className='colunas2'/>Espaços tranquilos</button><br/>
-<button href="3.1 Planejamento.html"><li className='colunas2'/>Planejamento</button><br/>
-<button href="4.1 Comunicação.html"><li className='colunas2'/>Comunicação</button><br/>
-<button href="5.1 Técnicas de relaxamento.html"><li className='colunas2'/>Técnicas de relaxamento</button><br/>
-<button href="6.1 Música ou ruído branco.html"><li className='colunas2'/>Música ou ruído branco</button><br/>
-<button href="7.1 Treinamento de dessensibilização.html"><li className='colunas2'/>Treinamento de dessensibilização</button><br/>
-</ul>
-
-<p>Cada pessoa é única, então pode ser necessário experimentar diferentes estratégias para encontrar o que funciona melhor em cada caso.</p>
-
-</div>
-
 <div class="sidebar">
       
 <h3>Estratégias para:</h3>
-<ul className='colunas1'>
- <button className='estrategias' ><li className='colunas3'/>Sons Altos</button><br /><br />
- <button className='estrategias' ><li className='colunas3'/>Mudança na Rotina</button><br /><br />
- <button className='estrategias' ><li className='colunas3'/>Rotas diferentes do previsto</button><br /><br />
- <button className='estrategias' ><li className='colunas3'/>Muitas pessoas em um ambiente</button><br /><br />
-</ul>
+
+<div className='colunas1'>
+
+ <button className='estrategias' >Sons Altos</button><br /><br />
+ <button className='estrategias' >Mudança na Rotina</button><br /><br />
+ <button className='estrategias' >Rotas diferentes do previsto</button><br /><br />
+ <button className='estrategias' >Muitas pessoas em um ambiente</button><br /><br />
+</div>
+
+<div className='ppp'>
+
+<h2  className='ppp3'>Sons Altos</h2>
+<p  className='ppp3'> Lidar com sons altos pode ser particularmente desafiador para pessoas autistas, mas algumas estratégias podem ajudar a minimizar o desconforto:</p>
+
+</div>
+
+<div className='colunas'>
+<button >Protetores auriculares</button><br />
+<button >Espaços tranquilos</button><br/>
+<button >Planejamento</button><br/>
+<button >Comunicação</button><br/>
+<button >Técnicas de relaxamento</button><br/>
+<button >Música ou ruído branco</button><br/>
+<button >Treinamento de dessensibilização</button><br/>
+
+</div>
+  
+
+<div className='ppp1'>
+<p>Cada pessoa é única, então pode ser necessário experimentar diferentes estratégias para encontrar o que funciona melhor em cada caso.</p>
+</div>
+
+</div>
+
+
 
 </div>
 </div>
