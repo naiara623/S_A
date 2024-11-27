517ef7328223c7fff7f5ab7a1df6eacb6ea55d6d
KAYLLANY KETYLLY DA SILVA LIMA <kayllany_lima@estudante.sesisenai.org.br>
2024-11-27 17:25:52 -0300

    Atualizações

--- Spectrum/src/pages/Tela_Perfil.jsx
+++ Spectrum/src/pages/Tela_Perfil.jsx
@@ -12,10 +12,10 @@ function Perfil() {
                 <div className='Ciano'></div>
 
                 <div className='Cinza'>
-                <img  className='foto-u' src="Samuel.png" alt="" />
-                <h1 className='nome'>Samuel Hernandes</h1>
-                <p className='idade'>Idade: 10 Anos</p>
-                <p className='genero'>Gênero: Masculino</p>
+                  <img  className='foto-u' src="Samuel.png" alt="" />
+                  <h1 className='nome'>Samuel Hernandes</h1>
+                  <p className='idade'>Idade: 10 Anos</p>
+                  <p className='genero'>Gênero: Masculino</p>
                 </div>
           </div>
 
@@ -29,21 +29,23 @@ function Perfil() {
                 <label className='gmail' htmlFor="">Gmail: email@gmail.com </label>
                 </div>
             </div>
-            <div>
+
+            <div className='conteine-button'>
                 <button className='butt-edi'>Editar</button> 
-                <button className='butt-dele'>Deletar</button>
+               <button className='butt-dele'>Deletar</button>
                 </div>
-          </div>
+           </div>
 
         <div className='Laranja'>
               <p className='detalhes'>DETALHES RELEVANTES SOBRE O USÚARIO:</p>
               <div className='marrom'>
-                <label htmlFor="">Qual o nivelamento do seu TEA?<br />Quando foi descorberto?</label><br />
-                <label htmlFor="">Quais as suas maiores necessidades? </label><br />
-                <label htmlFor="">Quais profissionais já o acompanham? </label><br />
-                <label htmlFor="">Quais são seus interesses? </label><br />
-                <label htmlFor="">Quais dificuldades já superou? </label><br />
-                <label htmlFor="">Tem alguma comorbidade? </label> 
+                <label>Qual o nivelamento do seu TEA?</label><br />
+                <label>Quando foi descorberto?</label><br />
+                <label>Quais as suas maiores necessidades? </label><br />
+                <label>Quais profissionais já o acompanham? </label><br />
+                <label>Quais são seus interesses? </label><br />
+                <label>Quais dificuldades já superou? </label><br />
+                <label>Tem alguma comorbidade? </label> 
                 </div>
         </div>
 
